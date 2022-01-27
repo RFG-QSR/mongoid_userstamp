6738e1cd4e88344913b1a242de209a0c4e2e7358
Armando Ruiz <armando.ruiz@industrie.co>
2022-01-27 15:44:10 +1100

    [N/A] Changed before_filter to before_action for Rails 5

--- lib/mongoid/userstamp/railtie.rb
+++ lib/mongoid/userstamp/railtie.rb
@@ -1,40 +1,46 @@
 # -*- encoding : utf-8 -*-
 
 module Mongoid
-module Userstamp
+  module Userstamp
+    class Railtie < Rails::Railtie
 
-  class Railtie < Rails::Railtie
-
-    # Include Mongoid::Userstamp::User into User class, if not already done
-    config.to_prepare do
-      Mongoid::Userstamp.user_classes.each do |user_class|
-        unless user_class.included_modules.include?(Mongoid::Userstamp::User)
-          user_class.send(:include, Mongoid::Userstamp::User)
-        end
-      end
-    end
-
-    # Add userstamp to models where Mongoid::Userstamp was included, but
-    # mongoid_userstamp was not explicitly called
-    config.to_prepare do
-      Mongoid::Userstamp.model_classes.each do |model_class|
-        unless model_class.included_modules.include?(Mongoid::Userstamp::Model)
-          model_class.send(:include, Mongoid::Userstamp::Model)
-        end
-      end
-    end
-
-    # Set current_user from controller reader method
-    ActiveSupport.on_load :action_controller do
-      before_filter do |c|
+      # Include Mongoid::Userstamp::User into User class, if not already done
+      config.to_prepare do
         Mongoid::Userstamp.user_classes.each do |user_class|
-          begin
-            user_class.current = c.send(user_class.mongoid_userstamp_user.reader)
-          rescue
+          unless user_class.included_modules.include?(Mongoid::Userstamp::User)
+            user_class.send(:include, Mongoid::Userstamp::User)
           end
+        end
+      end
+
+      # Add userstamp to models where Mongoid::Userstamp was included, but
+      # mongoid_userstamp was not explicitly called
+      config.to_prepare do
+        Mongoid::Userstamp.model_classes.each do |model_class|
+          unless model_class.included_modules.include?(Mongoid::Userstamp::Model)
+            model_class.send(:include, Mongoid::Userstamp::Model)
+          end
+        end
+      end
+
+      # Set current_user from controller reader method
+      # Changed before_filter to before_action for Rails 5
+      ActiveSupport.on_load :action_controller do
+        set_current = Proc.new do |c|
+          Mongoid::Userstamp.user_classes.each do |user_class|
+            begin
+              user_class.current = c.send(user_class.mongoid_userstamp_user.reader)
+            rescue
+            end
+          end
+        end
+
+        if respond_to?(:before_action)
+          before_action { |c| set_current.call(c)}
+        else
+          before_filter { |c| set_current.call(c)}
         end
       end
     end
   end
-end
 end
